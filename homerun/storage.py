3fb2e5a241f23f50894750cdcdcb9b3a4fb4ea65
Felix H. Dahlke <fhd@ubercode.de>
2014-03-28 19:04:59 +0100

    Don't require houses.json to exist

--- homerun/storage.py
+++ homerun/storage.py
@@ -1,11 +1,16 @@
 import json
+import os
 import spiders
+
+data_file = "houses.json"
 
 def save_houses(houses):
     marshalled = json.dumps(houses, indent=4, ensure_ascii=False)
-    file = open("houses.json", "w")
+    file = open(data_file, "w")
     file.write(marshalled.encode("utf-8"))
 
 def load_houses():
-    marshalled = open("houses.json").read()
+    if not os.path.isfile(data_file):
+        return []
+    marshalled = open(data_file).read()
     return json.loads(marshalled)
